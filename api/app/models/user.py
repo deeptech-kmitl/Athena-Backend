cbafc941fe19a0933d9cc71ff8f27ea6d7f99383
tigerza117 <noppawan.pakinsee@gmail.com>
2024-02-20 04:42:18 +0700

    fix: :card_file_box: orm not map to user

--- api/app/models/user.py
+++ api/app/models/user.py
@@ -15,7 +15,7 @@ class User(Base):
     # role = relationship("Role", back_populates="users")
 
     sessions = relationship("Session", back_populates="user", lazy=False)
-    # news = relationship("News", back_populates="author", lazy=False)
+    news = relationship("News", back_populates="author", lazy=False)
 
     created = Column(DateTime(timezone=True), server_default=func.now())
     updated = Column(DateTime(timezone=True), onupdate=func.now())
