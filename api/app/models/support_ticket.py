74e8e34b974cd4c2ba21fd0bdca6efc40dbd2b51
tigerza117 <noppawan.pakinsee@gmail.com>
2024-02-20 05:36:22 +0700

    feat: :sparkles: Init support ticket database model

---
+++ api/app/models/support_ticket.py
@@ -0,0 +1,53 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+
+from database.engine import Base
+
+
+class SupportTicket(Base):
+    __tablename__ = "support_tickets"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    title = Column(String, index=True)
+    priority = Column(Integer)
+
+    status = Column(String, default="open")
+    push_notification = Column(Boolean, default=False)
+    push_email = Column(Boolean, default=False)
+    mark_resolved = Column(Boolean, default=False)
+
+    assign_to_id = Column(Integer, ForeignKey("users.id"))
+    assign_to = relationship("User", back_populates="support_ticket_assign_tos")
+
+    events = relationship(
+        "SuppportTicketEvent", back_populates="support_ticket", lazy=True
+    )
+
+    created = Column(DateTime(timezone=True), server_default=func.now())
+    updated = Column(DateTime(timezone=True), onupdate=func.now())
+
+
+class SuppportTicketEvent(Base):
+    __tablename__ = "support_ticket_events"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    message_type = Column(String)
+    message = Column(String)
+
+    file_type = Column(String)
+    file = Column(String)
+
+    read = Column(Boolean, default=False)
+    reply = Column(Boolean, default=False)
+
+    user_id = Column(Integer, ForeignKey("users.id"))
+    user = relationship("User", back_populates="support_ticket_events")
+
+    support_ticket_id = Column(Integer, ForeignKey("support_tickets.id"))
+    support_ticket = relationship("SupportTicket", back_populates="events")
+
+    created = Column(DateTime(timezone=True), server_default=func.now())
+    updated = Column(DateTime(timezone=True), onupdate=func.now())
