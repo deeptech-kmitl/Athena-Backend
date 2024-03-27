add85d52c1a701ff43ff4019ed56594e088dcb00
tigerza117 <noppawan.pakinsee@gmail.com>
2024-03-28 01:07:13 +0700

    Update credit model

--- api/app/models/credit.py
+++ api/app/models/credit.py
@@ -9,14 +9,14 @@ class CreditTransaction(Base):
     __tablename__ = "credit_transactions"
 
     id = Column(Integer, primary_key=True, index=True)
-    amount = Column(Integer, primary_key=True, index=True)
-    detail = Column(String, primary_key=True, index=True)
+    amount = Column(Integer, index=True)
+    detail = Column(String, index=True)
 
-    user_id = Column(Integer, index=True)
-    user = relationship("User", back_populates="transactions")
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=True)
+    user = relationship("User", foreign_keys=[user_id])
 
-    target_id = Column(Integer, index=True)
-    target = relationship("User", back_populates="transactions_target")
+    target_id = Column(Integer, ForeignKey("users.id"), nullable=True)
+    target = relationship("User", foreign_keys=[target_id])
 
     created = Column(DateTime(timezone=True), server_default=func.now())
     updated = Column(DateTime(timezone=True), onupdate=func.now())
