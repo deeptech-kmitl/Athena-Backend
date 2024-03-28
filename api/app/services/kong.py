d8c8512d57f468d79dd31937388d02d03c675c98
tigerza117 <noppawan.pakinsee@gmail.com>
2024-03-28 14:49:27 +0700

    ✨ Kong API Service Support

---
+++ api/app/services/kong.py
@@ -0,0 +1,44 @@
+import json
+from dotenv import dotenv_values
+import requests
+
+config = dotenv_values(".env")
+
+KONG_API_URL = config["KONG_API_URL"]
+
+
+def create_service(name: str, tunnel: str):
+    payload = json.dumps(
+        {
+            "name": name,
+            "url": tunnel,
+        }
+    )
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded",
+    }
+
+    response = requests.request(
+        "POST",
+        KONG_API_URL + "/services",
+        headers=headers,
+        data=payload,
+    )
+
+    return response.json()
+
+
+def create_route(name: str, path: str):
+    payload = json.dumps({"name": name, "paths": path, "protocols": "https"})
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded",
+    }
+
+    response = requests.request(
+        "POST",
+        KONG_API_URL + "/services/" + name + "/routes",
+        headers=headers,
+        data=payload,
+    )
+
+    return response.json()
